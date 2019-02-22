15dcd0d448e780fde0b8c86d590979089190101d
TheOfficialSimon <simon.gutgesell@gmail.com>
2019-02-22 14:24:01 +0100

    Fixed theme setting when none was present

--- public_html/settings/script.js
+++ public_html/settings/script.js
@@ -48,7 +48,7 @@ function getSettings() {
         opt.value = n * 360 / themes;
         opt.innerHTML = "Theme " + (n + 1);
     }
-    theme.value = localStorage.theme || "0deg";
+    theme.value = localStorage.theme == true ? localStorage.theme : "0";
     // TODO: Add random theme
 }
 
